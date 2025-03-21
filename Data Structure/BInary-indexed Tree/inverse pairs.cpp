320d9b90c3c00f23eaa2e99ab683590ee0618d3f
彭志勇 <2457305628@qq.com>
2025-03-21 18:25:35 +0800

    update

--- Data Structure/BInary-indexed Tree/inverse pairs.cpp
+++ Data Structure/BInary-indexed Tree/inverse pairs.cpp
@@ -26,17 +26,21 @@ void update(int i, int val) {
 }
 
 // 求逆序对函数
-int sortedIndexs[N]; // 下标数组
+int idxs[N]; // 下标数组
+
 int inversePairs() {
-    iota(sortedIndexs, sortedIndexs + n, 1); // 下标从1开始
-    sort(sortedIndexs, sortedIndexs + n, [&](int i, int j) {
-        if (a[i] == a[j]) return i > j; // 元素相等时，按下标逆序（这样可以避免元素值一样时，被算进逆序对数量里面）
+    iota(idxs, idxs + n, 1); // 下标从1开始
+    sort(idxs, idxs + n, [&](int i, int j) {
+        // 元素相等时，按下标逆序（这样可以避免元素值一样时，被算进逆序对数量里面）
+        if (a[i] == a[j]) return i > j;
         return a[i] > a[j]; // 元素不相等时，按元素逆序
     });
     int count = 0;
     for (int i = 0; i < n; i++) {
-        count += query(sortedIndexs[i]); // 查询大于自己的元素中有多少元素的下标小于自己
-        update(sortedIndexs[i], 1);
+        count += query(idxs[i]); // 查询大于自己的元素中有多少元素的下标小于自己
+        update(idxs[i], 1);
     }
     return count;
 }
+
+// 另外一种写法是查询所有下标比自己小的元素里有多少元素大于自己，需要离散化
