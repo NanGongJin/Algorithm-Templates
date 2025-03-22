304690b44553afb103ca33f376129e48d13f2219
彭志勇 <2457305628@qq.com>
2025-03-22 14:05:44 +0800

    update

--- Graph/Shortest Path/Dijkstra.cpp
+++ Graph/Shortest Path/Dijkstra.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
-using vpii = vector<pii>;
+using pii = array<int, 2>;
 
 const int N = 1e5 + 5, inf = 0x7fffffff;
-vpii e[N];
-int n, m, dis[N], pre[N];
-
-void init() { // 初始化
-    for (int i = 0; i < n; i++) { // 节点从0开始编号
-        dis[i] = inf;
-        pre[i] = 0;
-    }
-}
+int n, m, dis[N], pre[N]; // 节点从0开始编号
+vector<pii> e[N];
 
 // 堆优化的 Dijkstra，平均时间复杂度O(mlogn)，最坏时间复杂度（稠密图）O(n²logn)
 void dijkstra(int s) {
-    init();
-    priority_queue<pii, vpii, greater<>> q; // first 为起点到 second 的距离
+    memset(dis, 0x3f, sizeof(dis)); // 初始化
+    memset(pre, 0, sizeof(pre));
+    priority_queue<pii, vector<pii>, greater<>> q; // p[0] 为起点到 p[1] 的距离
     dis[s] = 0, q.push({ 0, s });
-    while (!q.empty()) {
+    while (q.size()) {
         auto [d, u] = q.top(); q.pop();
         if (dis[u] < d) continue;
         for (auto& [v, w] : e[u])
@@ -33,7 +26,8 @@ void dijkstra(int s) {
 
 // 朴素的 Dijkstra，时间复杂度O(n²)
 void dijkstra(int s) { // 以 s 为起点
-    init();
+    memset(dis, 0x3f, sizeof(dis)); // 初始化
+    memset(pre, 0, sizeof(pre));
     bool done[N];
     memset(done, 0, sizeof(done));
     dis[s] = 0;
