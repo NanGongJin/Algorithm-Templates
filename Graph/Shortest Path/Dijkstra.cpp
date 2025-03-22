d27a6186fdae530cff7d612df8b4a814071c908b
彭志勇 <2457305628@qq.com>
2025-03-22 14:08:37 +0800

    update

--- Graph/Shortest Path/Dijkstra.cpp
+++ Graph/Shortest Path/Dijkstra.cpp
@@ -5,12 +5,12 @@ using pii = array<int, 2>;
 const int N = 1e5 + 5, inf = 0x7fffffff;
 int n, m, dis[N], pre[N]; // 节点从0开始编号
 vector<pii> e[N];
+priority_queue<pii, vector<pii>, greater<>> q; // p[0] 为起点到 p[1] 的距离
 
 // 堆优化的 Dijkstra，平均时间复杂度O(mlogn)，最坏时间复杂度（稠密图）O(n²logn)
 void dijkstra(int s) {
     memset(dis, 0x3f, sizeof(dis)); // 初始化
     memset(pre, 0, sizeof(pre));
-    priority_queue<pii, vector<pii>, greater<>> q; // p[0] 为起点到 p[1] 的距离
     dis[s] = 0, q.push({ 0, s });
     while (q.size()) {
         auto [d, u] = q.top(); q.pop();
@@ -28,8 +28,7 @@ void dijkstra(int s) {
 void dijkstra(int s) { // 以 s 为起点
     memset(dis, 0x3f, sizeof(dis)); // 初始化
     memset(pre, 0, sizeof(pre));
-    bool done[N];
-    memset(done, 0, sizeof(done));
+    bool done[N] {};
     dis[s] = 0;
     for (int i = n - 1; i--;) { // 只需 n - 1 次即可
         int u = -1, d = inf;
