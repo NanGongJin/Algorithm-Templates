c276d4f834edbee0536859fb737881060dd2b047
彭志勇 <2457305628@qq.com>
2025-05-09 16:17:01 +0800

    update

--- Data Structure/Block Data Structure/Mo's algorithm.cpp
+++ Data Structure/Block Data Structure/Mo's algorithm.cpp
@@ -3,39 +3,37 @@
 using namespace std;
 
 const int N = 1e5 + 5;
-int n, m, a[N], pos[N], ans[N], cnt[N]; // n个元素，m个查询，元素下标均从1开始
+int n, m, a[N], b[N], ans[N], cnt[N]; // n 个元素，m 个查询，元素下标从 0 开始
 
 struct node {
     int id, l, r;
+    
     bool operator<(node& x) {
-        if (pos[l] != pos[x.l])
-            return pos[l] < pos[x.l];
-        if (pos[l] & 1) return r > x.r; // 奇偶性优化
-        return r < x.r;
+        if (b[l] != b[x.l]) return l < x.l;
+        return b[l] & 1 ? r > x.r : r < x.r; // 奇偶性优化
     }
 } q[N];
 
 int ANS; // 区间内不同元素的数量
 
 void add(int x) {
-    cnt[a[x]]++;
-    if (cnt[a[x]] == 1) ANS++;
+    ANS += !cnt[a[x]]++;
 }
 
 void del(int x) {
-    cnt[a[x]]--;
-    if (cnt[a[x]] == 0) ANS--;
+    ANS -= !--cnt[a[x]];
 }
 
 void Mo() {
-    int block = sqrt(n);
-    for (int i = 1; i <= n; i++) pos[i] = (i - 1) / block + 1;
-    sort(q + 1, q + 1 + m);
-    for (int i = 1, l = 1, r = 0; i <= m; i++) { // q[i]: [l, r]
+    int sz = sqrt(n);
+    for (int i = 0; i < n; i++)
+        b[i] = i / sz;
+    sort(q, q + m);
+    for (int l = 0, r = -1, i = 0; i < m; i++) { // q[i]: [l, r]
+        while (l > q[i].l) add(--l); // 先加后减，保证元素数量不为负
+        while (r < q[i].r) add(++r);
         while (l < q[i].l) del(l++);
         while (r > q[i].r) del(r--);
-        while (l > q[i].l) add(--l);
-        while (r < q[i].r) add(++r);
         ans[q[i].id] = ANS;
     }
 }
--- Data Structure/Block Data Structure/block algrithm.cpp
+++ Data Structure/Block Data Structure/block algrithm.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 const int N = 1e6 + 5;
-int n, a[N]; // 下标从0开始
+int n, a[N]; // 下标从 0 开始
 /**
- * sz：块的大小，m：块的数量
- * i / sz：下标为 i 的元素的块号（从 0 开始）
+ * b[i]: 下标为 i 的元素所在的块号（从 0 开始）
  * st/ed[i]：第 i 块的开始/结束下标
  */
-int sz, m, st[N], ed[N];
+int b[N], st[N], ed[N];
 
 void init() {
-    sz = sqrt(n);
-    m = (n + sz - 1) / sz;
+    // sz: 块的大小，m：块的数量
+    int sz = sqrt(n), m = (n + sz - 1) / sz;
+    for (int i = 0; i < n; i++)
+        b[i] = i / sz;
     for (int i = 0; i < m; i++) {
         st[i] = i * sz;
         ed[i] = i * sz + sz - 1;
@@ -20,40 +21,39 @@ void init() {
     ed[m - 1] = n - 1;
 }
 
-// sum[i]：第 i 个块的区间和
+// sum[i]：i 号块的区间和
 int sum[N], add[N];
 
 void init_sum() {
-    for (int i = 0; i < m; i++)
-        for (int j = st[i]; j <= ed[i]; j++)
-            sum[i] += a[j];
+    for (int i = 0; i < n; i++)
+        sum[b[i]] += a[i];
 }
 
 // 让 [l, r] 内的每个元素加 d
 void update(int l, int r, int d) {
-    int p = l / sz, q = r / sz;
-    if (p == q) {
+    if (b[l] == b[r]) {
         for (int i = l; i <= r; i++) a[i] += d;
-        sum[p] += d * (r - l + 1);
+        sum[b[l]] += d * (r - l + 1);
         return;
     }
-    for (int i = l; i <= ed[p]; i++) a[i] += d;
-    sum[p] += d * (ed[p] - l + 1);
-    for (int i = p + 1; i <= q - 1; i++) add[i] += d;
-    for (int i = st[q]; i <= r; i++) a[i] += d;
-    sum[q] += d * (r - st[q] + 1);
+    for (int i = l; i <= ed[b[l]]; i++) a[i] += d;
+    sum[b[l]] += d * (ed[b[l]] - l + 1);
+    for (int i = b[l] + 1; i <= b[r] - 1; i++) add[i] += d;
+    for (int i = st[b[r]]; i <= r; i++) a[i] += d;
+    sum[b[r]] += d * (r - st[b[r]] + 1);
 }
 
 // 查询 [l, r] 内每个元素的和
 int query(int l, int r) {
-    int p = l / sz, q = r / sz, res = 0;
-    if (p == q) {
+    int res = 0;
+    if (b[l] == b[r]) {
         for (int i = l; i <= r; i++) res += a[i];
         return res;
     }
-    res = add[p] * (ed[p] - l + 1) + add[q] * (r - st[q] + 1);
-    for (int i = l; i <= ed[p]; i++) res += a[i];
-    for (int i = p + 1; i < q - 1; i++) res += sum[i];
-    for (int i = st[q]; i <= r; i++) res += a[i];
+    res = add[b[l]] * (ed[b[l]] - l + 1) + add[b[r]] * (r - st[b[r]] + 1);
+    for (int i = l; i <= ed[b[l]]; i++) res += a[i];
+    for (int i = b[l] + 1; i <= b[r] - 1; i++)
+        res += sum[i] + add[i] * (ed[i] - st[i] + 1);
+    for (int i = st[b[r]]; i <= r; i++) res += a[i];
     return res;
 }
