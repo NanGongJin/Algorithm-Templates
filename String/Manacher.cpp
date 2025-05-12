bcec7d498a601277865dee0433edb12bb620b87b
彭志勇 <2457305628@qq.com>
2025-05-12 17:21:55 +0800

    update

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -2,48 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+const int N = 1e5 + 5;
+int n, p[2 * N] {1};
+string s, ns;
 
+/**
+ * p[i] 表示以 i 为中心的最长回文半径
+ * max(p) - 1 即为最长回文子串的长度
+ * 在原字符串中的起始位置为 (i - p[i] + 2) / 2
+ */
 string manacher() {
-    // 预处理字符串，插入特殊字符 '#'
-    string processed = "#";
+    ns = "#";
     for (char c : s)
-        processed += c + "#"s;
-
-    int n = processed.size();
-    vector<int> p(n, 0);  // p[i] 表示以 i 为中心的最大回文半径
-    int center = 0, right = 0; // 当前回文串的中心和右边界
-    int max_len = 0, max_center = 0;
-
-    for (int i = 0; i < n; i++) {
-        // 利用对称性优化计算
-        int mirror = 2 * center - i;
-        if (i < right)
-            p[i] = min(right - i, p[mirror]);
-
-        // 中心扩展
-        int l = i - (p[i] + 1);
-        int r = i + (p[i] + 1);
-        while (l >= 0 && r < n && processed[l] == processed[r]) {
-            p[i]++;
-            l--;
-            r++;
-        }
-
-        // 更新最右边界和中心
-        if (i + p[i] > right) {
-            center = i;
-            right = i + p[i];
-        }
-
-        // 记录最长回文子串
-        if (p[i] > max_len) {
-            max_len = p[i];
-            max_center = i;
-        }
+        ns += c + "#"s;
+    int c = 0, mx = 0; // 最右边界/最长回文子串的中心
+    for (int i = 1; i < ns.size(); i++) {
+        p[i] = i < c + p[c] ? min(c + p[c] - i, p[2 * c - i]) : 1;
+        while (
+            0 <= i - p[i]
+            && i + p[i] <= 2 * n
+            && ns[i + p[i]] == ns[i - p[i]]
+        ) p[i]++;
+        if (i + p[i] > c + p[c]) c = i;
+        if (p[i] > p[mx]) mx = i;
     }
-
-    // 转换为原字符串的起始位置
-    int start = (max_center - max_len) / 2;
-    return s.substr(start, max_len);
+    return s.substr((mx - p[mx] + 2) / 2, p[mx] - 1);
 }
