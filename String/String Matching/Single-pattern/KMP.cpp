b7387ba93adf8f34df4ba30a9d1be8de73d0f5a7
南宫谨 <118327380+NanGongJin@users.noreply.github.com>
2025-07-19 22:40:22 +0800

    Update KMP.cpp

--- String/String Matching/Single-pattern/KMP.cpp
+++ String/String Matching/Single-pattern/KMP.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 /**
- * Next[j]：以下标为j的字符结尾的前缀字符串的最大公共前后缀字符串长（前后缀字符串长度不能等于j + 1）
+ * Next[j]：以下标为j的字符结尾的前缀字符串的最大公共前后缀字符串长（不能等于j + 1）
  * 也就是可以跳过的字符数量
  * Next[j - 1]是s[i]和p[j]不相等时，新的j的位置
  * 该数组也可用于找所有的公共前后缀
@@ -14,22 +14,21 @@ int Next[N];
 /**
  * dp 法求所有前缀的最长公共前后缀
  * j 是上一个字符串的最长公共前后缀长，同时也是最长公共前后缀所对应的前缀的后一个字符的下标
- * 当p[j] != p[i]时，说明p[0..j]不是p[0..i]的最长公共前后缀
- * 又因为p[0..j-1]就是p[i-j..i-1]，所以p[0..i]的最长公共前后缀也是p[i-j..i]的最长公共前后缀
- * 也就是p[0..j-1] + p[i]的最长公共前后缀
+ * 当p[j] != p[i]时，说明p[0...j]不是p[0...i]的最长公共前后缀
+ * 又因为p[0...j-1]就是p[i-j...i-1]，所以p[0...i]的最长公共前后缀也是p[i-j...i]的最长公共前后缀
+ * 也就是p[0...j-1] + p[i]的最长公共前后缀
  */
-void getFail(string& p) {
-    for (int i = 1; i < p.length(); i++) {
-        int j = Next[i - 1];
+void getFail(string p) {
+    for (int i = 1, j = 0; i < p.length(); i++) {
         while (j && p[j] != p[i]) j = Next[j - 1];
-        Next[i] = j + (p[i] == p[j]);
+        Next[i] = j += p[i] == p[j];
     }
 }
 
-int kmp(string& s, string& p) {
+int kmp(string s, string p) {
     getFail(p);
-    int cnt = 0, j = 0;
-    for (int i = 0; i < s.length(); i++) {
+    int cnt = 0;
+    for (int i = 0, j = 0; i < s.length(); i++) {
         while (j && s[i] != p[j]) j = Next[j - 1];
         j += s[i] == p[j];
         if (j == p.length()) { // 找到了
